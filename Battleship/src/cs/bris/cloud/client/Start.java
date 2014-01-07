2abfd89b746f014117050d7330cd36b1b9838dc5
Callum Steele <callum@steele.uk.com>
2014-01-07 23:51:28 +0000

    Added login form.

--- Battleship/src/cs/bris/cloud/client/Start.java
+++ Battleship/src/cs/bris/cloud/client/Start.java
@@ -4,22 +4,45 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Start extends Composite {
 	public Start() {
-		VerticalPanel panel = new VerticalPanel();
-		final Button startButton = new Button("Start");
-		panel.add(startButton);
+		VerticalPanel content = new VerticalPanel();
+		
+		// create user name input UI
+		HorizontalPanel usernamePanel = new HorizontalPanel();
+		final Label usernameLabel = new Label("Username:");
+		final TextBox usernameTextBox = new TextBox();
+		usernamePanel.add(usernameLabel);
+		usernamePanel.add(usernameTextBox);
+		
+		// create password input UI
+		HorizontalPanel passwordPanel = new HorizontalPanel();
+		final Label passwordLabel = new Label("Password:");
+		final PasswordTextBox passwordTextBox = new PasswordTextBox();
+		passwordPanel.add(passwordLabel);
+		passwordPanel.add(passwordTextBox);
+		
+		final Button loginButton = new Button("Login");
+		
+		content.add(usernamePanel);
+		content.add(passwordPanel);
+		content.add(loginButton);
 		
 		// Add a handler to close the DialogBox
-		startButton.addClickHandler(new ClickHandler() {
+		loginButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				System.out.println("Start button clicked, huzzah!");
+				System.out.println("Login button clicked, huzzah!");
+				//System.out.println("Username: " + usernameTextBox.getText() + " Password: " + passwordTextBox.getText());
 				ContentController.getInstance().setContent(new Finish());
 			}
 		});
 		
-		initWidget(panel);
+		initWidget(content);
 	}
 }
